19ff4eb5edf6237ba5c0974ff7f8cd2b8e7f1c09
emilyTheseond <esegundo02@gmail.com>
2024-02-23 16:29:43 -0600

    set up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 
 const App = () => {
   return (
     <div>
+      <Nav></Nav>
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Main>
+      <Footer></Footer>
     </div>
   )
 }
