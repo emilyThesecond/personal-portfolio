4d943b2e8fda9dd2c8f5dbed45bd91930378aad3
emilyTheseond <esegundo02@gmail.com>
2024-02-23 16:34:49 -0600

    set up navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <div>
-      Nav
+        <NavLink to='/'>Home</NavLink>
+        <NavLink>Projects</NavLink>
+        <NavLink>About</NavLink>
+        <NavLink>Contact</NavLink>
     </div>
   )
 }
