9e6746ea2a8636ebdfa9603f8e2327f9aabe28ec
emilyTheseond <esegundo02@gmail.com>
2024-02-23 16:39:33 -0600

    set up routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+import Nav from "./components/Nav"
 
 
 const App = () => {
   return (
     <div>
-      <header></header>
+      <header>
+        <Nav/>
+      </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </main>
       <footer></footer>
