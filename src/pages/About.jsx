e6382711a0ab8d6890962f517d90c975dcb28e67
emilyTheseond <esegundo02@gmail.com>
2024-02-26 10:32:40 -0600

    revise about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,7 @@ const About = () => {
         <div>
             
       <p>Hi! Welcome to my Portfolio, Im Emily.</p>
-      <h6>My Journey Into loving Software engineer started when I was a little girl. Coding games in the computer lab sparked an interest that i carried for years, but felt i fell to short of that dream. Now, after years of self-doubt, I decided to dive into this amazing world. Being able to use my creativity, push myself to create innovative solutions, and always needing to learn more are elements that needed in a career, and that I got! I truley enjoy building and learning new things and I cant wait to keep gaining more knowledge. </h6>
-      
+      <h6>My Journey Into loving Software engineer started when I was a little girl. Coding games in the computer lab sparked an interest that i carried for years, though at times I felt I fell short of pursuing that dream. Now, after years of self-doubt, I decided to dive into this amazing world. The ability to harness my creativity, challenge myself with innovative solutions, and constantly thirst for knowledge are all aspects I find essential in a career, and I'm fortunate to have found them in software engineering. I truly enjoy the process of building and continually learning new things, and I cant wait to keep gaining more knowledge.</h6>
       <h6>other things about me: </h6>
       <h6>I am a big hobby person, name any art or craft, I love. My second passion is pottery, (Hence my "Glaze Cookbook" app) although an expensive craft, like in coding, very fullfilling building something from scratch and calling it yours!</h6>
         </div>
