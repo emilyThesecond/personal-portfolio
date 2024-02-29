654150259b8fc9fc07c91baf39421a2dcb1ef8c6
emilyTheseond <esegundo02@gmail.com>
2024-02-29 16:52:12 -0600

    change to social icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className='home'>
-        <div className='name'>
+      <div className='name'>
         <p>Emily Segundo</p>
-        {/* <p>EMILY SEGUNDO</p> */}
         <h3>SOFTWARE ENGINEER</h3>
+        <h4>Technology:</h4>
 
-        </div>
+      </div>
+      <div className='buttons'>
         <Link to='/contact'>
-    <button className='back-button'>C O N T A C T</button>
-    </Link>
-     <Link to='/projects'>
-    <button className='next-button'>P R O J E C T S</button>
-    </Link>
+          <button className='back-button'>CONTACT</button>
+        </Link>
+        <Link to='/projects'>
+          <button className='next-button'>PROJECTS</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
