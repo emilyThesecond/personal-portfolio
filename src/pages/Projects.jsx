a4973ce389db2f9089928c5cb81a2ad476dba394
emilyTheseond <esegundo02@gmail.com>
2024-02-29 11:53:01 -0600

    project page styling

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,29 +5,28 @@ const Projects = () => {
   return (
     <div className='project-page'>
     <div className="projects-container">
-      <p>Projects</p>
+      <h1 className='project-header'>P R O J E C T S</h1>
       <div className="project">
         <h1>The Strokes Fans</h1>
-        <button></button>
-        <a href="https://the-strokes-fan-page.vercel.app/">live site</a>
-        <a href="https://github.com/emilyThesecond/the-strokes-client">Github</a>
-        <h3>Details: </h3>
+        <a href="https://the-strokes-fan-page.vercel.app/"><button>live site</button></a>
+        <a href="https://github.com/emilyThesecond/the-strokes-client"><button>Gitub</button></a>
+        <h3>Details: Welcome to the ultimate destination for all things related to The Strokes! 🎸🎶 Dive into the world of this iconic indie rock band and connect with fellow fans. Stay updated on the latest news, tour dates, music releases, and explore their extensive setlists from past performances. Let's celebrate the music and legacy of The Strokes together! This fan page is powered by a full stack MERN (MongoDB, Express.js, React.js, Node.js) application, providing a seamless and interactive experience for fans worldwide.</h3>
         <h4>Technologies Used: MongoDB, Express, React, Node.js</h4>
         <img src="https://i.imgur.com/ks0HzMr.png" alt="Project 1" />
       </div>
       <div className="project">
         <h1>Glaze Cookbook</h1>
         <a href=""></a>
-        <a href="https://github.com/emilyThesecond/glaze-cookbook">Github</a>
-        <h3>Details</h3>
+        <a href="https://github.com/emilyThesecond/glaze-cookbook"><button>Gitub</button></a>
+        <h3>Details: A full-stack community application for pottery enthusiasts to share real-life experiences, insights on brands, colors, glaze combinations, and techniques, fostering collaboration and support among members.</h3>
         <h4>Technologies Used: MongoDB, Express, Node.js, JavaScript</h4>
         <img src="https://i.imgur.com/s0NrMV0.png" alt="Project 2" />
       </div>
       <div className="project">
         <h1>Habitualize</h1>
-        <a href="https://habitualize-07273966efbf.herokuapp.com/">Live Site</a>
-        <a href="https://github.com/harmonica23/habitualize">Github</a>
-        <h3>Details</h3>
+        <a href="https://habitualize-07273966efbf.herokuapp.com/"><button>Live site</button></a>
+        <a href="https://github.com/harmonica23/habitualize"><button>Gitub</button></a>
+        <h3>Details: Habitualize is a Habit Tracker application that helps users build positive habits and break unwanted ones. It provides a user-friendly interface for tracking habits and setting goals. With Habitualize, users can login to their personal tracker, customize their habits and visualize their progress over time, watching their streaks grow as they build or break habits.</h3>
         <h4>Technologies Used: Python, Django, PostgreSQL</h4>
         <img src="https://i.imgur.com/86E64Nb.png" alt="Project 3" />
       </div>
